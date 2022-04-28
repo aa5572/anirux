6ee3a5104d1c35f5b97bdfe9bca99b04e79cf2f7
ANIRUDH SHARMA <91799211+aa5572@users.noreply.github.com>
2022-04-28 10:13:44 +0530

    Add files via upload
    If a four digit number is input through the keyboard, I have written a
    C program to obtain the sum of the first and last digit of the
    number.

---
+++ main.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int f,l,n,sum=0;
+    scanf("%d",&n);
+    l=n%10;
+    while (n>0)
+    {f=n%10;
+    n=n/10;
+	}
+    sum=l+f;
+    printf("sum is %d",sum);
+    
+    return 0;
+}
+
+
+
+
+
+
